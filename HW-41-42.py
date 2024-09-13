ed1d92a49a7bbf456478b497ea32b63ebdeeee48
Sergio <sergio11iw@mail.ru>
2024-09-13 18:56:52 +0300

    final

--- HW-41-42.py
+++ HW-41-42.py
@@ -1,24 +1,19 @@
 class Car:
-    def __init__(self, name):
-        self.name = name
-        self.color = 'red'
-        self.fuel = 40
-        self.rate = 10
-        self.mileage = 0
+    def __init__(self, color, fuel, rate, mileage=0):
+        self.color = color
+        self.fuel = fuel
+        self.rate = rate
+        self.mileage = mileage
     def drive(self, km):
-        path = 100*self.fuel / self.rate
-        if km <= path:
-            self.fuel -= km / self.rate
-            print(f'Мы проехали {km} км')
+        neeg_fuel = (km*self.rate)/100
+        if neeg_fuel <= self.fuel:
+            print((f'Мы проехали {km} км'))
+            self.fuel -= neeg_fuel
+            self.mileage += km
         else:
-            self.fuel = 0
             print('Не хватает топлива')
-    def get_mileage(self, km):
-        return km
+    def get_mileage(self):
+        return self.mileage
 
 
-car1 = Car(name='Mini')
-car1.drive(500)
-print(car1.fuel)
-car1.get_mileage(500)
-print(km)
+
